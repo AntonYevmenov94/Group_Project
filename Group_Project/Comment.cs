60e34848c33521194da977fb5ac49329b6a865cd
klim86klim <klim86kr@gmail.com>
2020-06-11 20:42:39 +0300

    Добавлены 3 класса

---
+++ Group_Project/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public int PeopleId { get; set; }
+        [ForeignKey(nameof(PeopleId))]
+        public virtual People People { get; set; }
+
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+
+    }
+}
---
+++ Group_Project/Discipline.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project
+{
+    [Table("Disciplines")]
+    public class Discipline
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public virtual ICollection<Vacancy> Vacancies { get; set; }
+        public virtual ICollection<Technology> Technologies { get; set; }
+
+        public Discipline()
+        {
+            Vacancies = new HashSet<Vacancy>();
+            Technologies = new HashSet<Technology>();
+        }
+
+    }
+}
