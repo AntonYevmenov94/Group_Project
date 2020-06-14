fbf5362bdc7796d4a6540c0f8d07af3da04870f6
Nishtra <id.yuuko@gmail.com>
2020-06-14 18:42:25 +0300

    Добавлен базовый класс для ValueConverter и несколько конвертеров

---
+++ Group_Project/Helpers/ValueConverterMarkupBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Group_Project.Converters
+{
+    /// <summary>
+    /// Базовый класс для конвертеров, который позволяет использовать конвертер в XAML не объявляя его как ресурс.
+    /// </summary>
+    /// <typeparam name="T">Тип ValueConverter'a, который делается доступным в XAML</typeparam>
+    public abstract class ValueConverterMarkupBase<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        /// <summary>
+        /// Статический экземпляр конвертера
+        /// </summary>
+        private static T _converter = null;
+
+        /// <summary>
+        /// Возвращает статический экземпляр конвертера
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _converter ?? (_converter = new T());
+        }
+
+
+        #region IValueConverter
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+        #endregion
+    }
+}
