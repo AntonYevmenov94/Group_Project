bb777f82856a89207304f1d5d6ce1799c2c5ceb1
Nishtra <id.yuuko@gmail.com>
2020-06-16 20:51:35 +0300

    Добавлено окно для отображения/редактирования Предмета (Discipline).

---
+++ Group_Project/Views/WindowDisciplineEdit.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Group_Project.Views
+{
+    /// <summary>
+    /// Interaction logic for WindowDisciplineEdit.xaml
+    /// </summary>
+    public partial class WindowDisciplineEdit : Window
+    {
+        public WindowDisciplineEdit()
+        {
+            InitializeComponent();
+        }
+    }
+}
