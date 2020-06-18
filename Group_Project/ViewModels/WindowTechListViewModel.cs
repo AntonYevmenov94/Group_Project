ac00600c22091a006bec6ae6cfc84870e4039e2d
Nishtra <id.yuuko@gmail.com>
2020-06-18 19:47:36 +0300

    Добавлены свойства и коментарии к View-Model'ям, которые работают с WindowGenericList.

--- Group_Project/ViewModels/WindowTechListViewModel.cs
+++ Group_Project/ViewModels/WindowTechListViewModel.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Group_Project.ViewModels
 {
+    /// <summary>
+    /// Модель-представления списка технологий. Привязывается к окну WindowGenericList, которое делит с
+    /// WindowDepartmentListViewModel, WindowDisciplineListViewModel, WindowPositionListViewModel 
+    /// </summary>
     public class WindowTechListViewModel : BaseViewModel
     {
+        // название окна (можно установить в конструкторе)
+        public string WindowTitle { get; set; }
+        // название списка элементов (текст label над listbox'ом) (можно установить в конструкторе)
+        public string ListTitle { get; set; }
+        // коллекция элементов, которые отображаются в списке
+        public ObservableCollection<Technology> ListItems { get; set; }
+
         public WindowTechListViewModel(
             IAuthService accountService,
             IDbContextProvider dbContextProvider,
