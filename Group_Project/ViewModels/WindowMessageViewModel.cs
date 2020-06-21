0ece831a31f83f22afca893770ddb15fce2e3f7c
Nishtra <id.yuuko@gmail.com>
2020-06-22 00:11:29 +0300

    Добавлены окна и их ViewModel'и для подтверждения дейтсвия и демонстрации сообщения

---
+++ Group_Project/ViewModels/WindowMessageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Group_Project.ViewModels
+{
+    public class WindowMessageViewModel : BaseDialogViewModel
+    {
+        public string MessageText { get; set; }
+        public bool? DialogResult { get; private set; }
+
+        public ICommand CmdConfirm { get; set; }
+        
+        #region Constructor
+
+        public delegate WindowMessageViewModel Factory(string messageText);
+
+        public WindowMessageViewModel(
+            IAuthService authService,
+            IDbContextProvider dbContextProvider,
+            IDialogService dialogService,
+            ILogger logger,
+            ILogMessageBuilder logMessageBuilder,
+            string messageText)
+            : base(authService, dbContextProvider, dialogService, logger, logMessageBuilder)
+        {
+            MessageText = messageText;
+
+            CmdConfirm = new RelayCommand(new Action<object>(DialogConfirm));
+        }
+        #endregion
+
+
+        #region Command implementation
+
+        private void DialogConfirm(object obj = null)
+        {
+            DialogResult = true;
+            OnConfirm.Invoke();
+        }
+        #endregion
+    }
+}
